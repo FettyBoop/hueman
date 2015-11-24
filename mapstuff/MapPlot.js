4e741b9bb3beb6b6778431b5e02ab42d221395c9
angelalin2004 <adl040@ucsd.edu>
2015-11-24 09:56:56 -0800

    clicking on map now changes the x/y of the latest point, but doesn't get placed into map quite right

--- mapstuff/MapPlot.js
+++ mapstuff/MapPlot.js
@@ -11,7 +11,6 @@ map.src = "./img/map.png";
 var blobs = [];
 var clicked = false;
 
-
 /* Fit the canvas properly inside the window
 1000 * 550 are the "baseline" dimensions */
 function doResize()
@@ -56,6 +55,9 @@ function main()
 		});
 	});
 	
+	Parse.initialize("mEmM0UeRE8GX5hYcuI3Z8Yao4bT4Z7wTWyjOImvt", "G5gLmYiSdDo9YNHF56Rrom15e7VJyGYmUYlcu7f9");
+	var MapPoint = Parse.Object.extend("MapPoint");
+
 	// Click to choose a location
 	canvas.addEventListener("click", function(e)
 	{
@@ -65,6 +67,30 @@ function main()
 		clicked = true;
 		var x = e.pageX - canvas.offsetLeft;
 		var y = e.pageY - canvas.offsetTop;
+
+		
+		var query = new Parse.Query(MapPoint);
+		// Sorts the results in descending order by the Created Date field
+		query = query.addDescending("createdAt");
+
+		query.first().then(function(object){
+			object.set("x", x);
+			object.set("y", y);
+			console.log(object.get("x") + " " + object.get("y") + " " + object.get("color"));
+			
+			object.save(null, {
+			  success: function(object) {
+			    // Execute any logic that should take place after the object is saved.	    
+			  },
+			  error: function(object, error) {
+			    // Execute any logic that should take place if the save fails.
+			    // error is a Parse.Error with an error code and message.
+			    alert("error occurred while saving");
+			  }
+			});
+			
+		});
+		
 
 		getData();
 		var ctx = drawCanvas();
